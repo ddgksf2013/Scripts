9f76521ee0db7fc53d703bda43373246526b7086
ddgksf2013 <116851128+ddgksf2013@users.noreply.github.com>
2022-11-30 19:28:14 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -47,6 +47,6 @@ hostname = ad.12306.cn
 
 
 
-var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList&&(1==ddgksf2013.materialsList.length?(ddgksf2013.materialsList[0].filePath="",ddgksf2013.advertParam.skipTime=10,ddgksf2013.advertParam.skipTimeAgain=5,ddgksf2013.advertParam.showSkipBtn=-1):1<ddgksf2013.materialsList.length&&(ddgksf2013.materialsList=[{}])),$done({body:JSON.stringify(ddgksf2013)});
+var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList&&(1==ddgksf2013.materialsList.length?(ddgksf2013.materialsList[0].filePath="",ddgksf2013.advertParam.skipTime=1,ddgksf2013.advertParam.skipTimeAgain=5,ddgksf2013.advertParam.showSkipBtn=-1):1<ddgksf2013.materialsList.length&&(ddgksf2013.materialsList=[{}])),$done({body:JSON.stringify(ddgksf2013)});
 
 
