79a42a94c39be4fa82d6d2fbcc3f23b9a6e89b42
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2022-12-02 17:13:02 +0800

    fix the bug

--- bilibili_json.js
+++ bilibili_json.js
@@ -151,19 +151,7 @@ if (magicJS.read(blackKey)) {
                         delete obj["data"]["sections_v2"][index].tip_icon;
                         delete obj["data"]["sections_v2"][index].tip_title;
                         //2022-02-16 add by ddgksf2013
-                        for (let ii = 0; ii < obj["data"]["sections_v2"].length; ii++) {
-                            if (obj.data.sections_v2[ii].title == "推荐服务" || obj.data.sections_v2[ii].title == "推薦服務") {
-                                //obj.data.sections_v2[ii].items[0].title='\u516C\u773E\u865F';
-                                //obj.data.sections_v2[ii].items[1].title='\u58A8\u9B5A\u624B\u8A18';
-                            }
-                            if (obj.data.sections_v2[ii].title == "更多服務" || obj.data.sections_v2[ii].title == "更多服务") {
-                                if (obj.data.sections_v2[ii].items[0].id == 500) {
-                                    //obj.data.sections_v2[ii].items[0].title='\u516C\u773E\u865F';
-                                }
-                                if (obj.data.sections_v2[ii].items[1].id == 501) {
-                                    //obj.data.sections_v2[ii].items[1].title='\u58A8\u9B5A\u624B\u8A18';
-                                }
-                            }
+                        for (let ii = 0; ii < obj["data"]["sections_v2"].length; ii++) {                                                        
                             if (obj.data.sections_v2[ii].title == "创作中心" || obj.data.sections_v2[ii].title == "創作中心") {
                                 delete obj.data.sections_v2[ii].title;
                                 delete obj.data.sections_v2[ii].type;
@@ -255,7 +243,7 @@ if (magicJS.read(blackKey)) {
                             module.items = module.items.filter((i) => i.blink.indexOf("www.bilibili.com") == -1);
                         }
                         if (module.style.startsWith("tip")) {
-                            module.items = null;
+                            module.items = [];
                         }
                     });
                     body = JSON.stringify(obj);
@@ -276,7 +264,7 @@ if (magicJS.read(blackKey)) {
                             module.items = module.items.filter((i) => i.blink.indexOf("www.bilibili.com") == -1);
                         }
                         if (module.style.startsWith("tip")) {
-                            module.items = null;
+                            module.items = [];
                         }
                     });
                     body = JSON.stringify(obj);
@@ -321,7 +309,7 @@ if (magicJS.read(blackKey)) {
             case /^https:\/\/app\.bilibili\.com\/x\/v2\/splash\/list/.test(magicJS.request.url):
                 try {
                     let obj = JSON.parse(magicJS.response.body);
-                    if (obj.data) {
+                    if (obj.data&&obj.data.list) {
                         for (let item of obj["data"]["list"]) {
                             item["duration"] = 0;
                             // 显示时间
