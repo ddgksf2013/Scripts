5b42de7b41b32e43b500a4d297ef350ea6b44748
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2022-12-10 22:04:22 +0800

    Update weibo_json.js

--- weibo_json.js
+++ weibo_json.js
@@ -1,12 +1,14 @@
-/*
-应用名称：自用微博国际版去广告脚本
-脚本作者：Cuttlefish
-微信账号：公众号墨鱼手记
-更新时间：2022-12-10
-脚本版本：(0.0.8)
-通知频道：https://t.me/ddgksf2021
-问题反馈：ddgksf2013@163.com
-*/
+/********************************************************
+
+*应用名称：自用微博国际版去广告脚本
+*脚本作者：Cuttlefish
+*微信账号：公众号墨鱼手记
+*更新时间：2022-12-10
+*脚本版本：(0.0.10)
+*通知频道：https://t.me/ddgksf2021
+*问题反馈：ddgksf2013@163.com
+
+*******************************************************/
 const mainConfig = {};
 const modifyCardsUrls = ['/cardlist', 'video/community_tab', '/searchall'];
 const modifyTimeUrls = ['statuses/friends_timeline', 'statuses/unread_hot_timeline', 'groups/timeline'];
@@ -37,10 +39,7 @@ function modifiedSearchTopic(data) {
 	data = {"data":[],"info":"","retcode":0,"ext":{"search_hot_simple":{"title":"\u70ed\u95e8\u641c\u7d22","desc":"","more":"\u66f4\u591a\u70ed\u641c"},"search_hot":{"title":"\u5fae\u535a\u70ed\u641c\u699c","desc":"\u5b9e\u65f6\u70ed\u70b9\uff0c\u6bcf\u5206\u949f\u66f4\u65b0\u4e00\u6b21","more":""},"search_city":{"title":"\u540c\u57ce\u70ed\u70b9","desc":"","more":""}}};
 }
 function modifiedUserCenter(data) {
-	if(!data.data) {
-		return data;
-	}
-	if(data.data.length===0){
+	if(!data.data||data.data.length===0) {
 		return data;
 	}
 	if(data.data.cards){
