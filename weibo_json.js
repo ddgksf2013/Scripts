0588cf8729474a4bb158967fd1a384b3ca445c8d
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2022-12-12 23:47:45 +0800

    Update weibo_json.js

--- weibo_json.js
+++ weibo_json.js
@@ -8,7 +8,6 @@
 > 贡献投稿：https://t.me/ddgksf2013_bot
 > 原作者库：https://github.com/zmqcherish
 > 问题反馈：ddgksf2013@163.com
-> 脚本版本：V2.0.15
 > 特别提醒：如需转载请注明出处，谢谢合作！
 > 脚本声明：特别感谢Zmqcherish的付出，本脚本只是在他原创脚本的基础上优化自用
 > 特别说明：⚠️⚠️⚠️
@@ -18,7 +17,7 @@
 ***********************************/	  
 
 
-const version = 'V2.0.15';
+const version = 'V2.0.16';
 
 /*主要的选项配置*/
 const mainConfig = {
@@ -137,6 +136,7 @@ function getModifyMethod(url) {
 /*国际版SearchTopic*/
 function modifiedSearchTopic(data) {
 	data = {"data":[],"info":"","retcode":0,"ext":{"search_hot_simple":{"title":"\u70ed\u95e8\u641c\u7d22","desc":"","more":"\u66f4\u591a\u70ed\u641c"},"search_hot":{"title":"\u5fae\u535a\u70ed\u641c\u699c","desc":"\u5b9e\u65f6\u70ed\u70b9\uff0c\u6bcf\u5206\u949f\u66f4\u65b0\u4e00\u6b21","more":""},"search_city":{"title":"\u540c\u57ce\u70ed\u70b9","desc":"","more":""}}};
+	return data;
 }
 /*国际版用户中心*/
 function modifiedUserCenter(data) {
