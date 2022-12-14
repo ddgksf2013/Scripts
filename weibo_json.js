11d532b6eebfc27782e163cbfddc159304a22ae2
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2022-12-14 13:11:24 +0800

    d

--- weibo_json.js
+++ weibo_json.js
@@ -3,7 +3,7 @@
 > 应用名称：墨鱼自用微博&微博国际版净化
 > 脚本作者：@Zmqcherish, @Cuttlefish
 > 微信账号：墨鱼手记
-> 更新时间：2022-12-13
+> 更新时间：2022-12-14
 > 通知频道：https://t.me/ddgksf2021
 > 贡献投稿：https://t.me/ddgksf2013_bot
 > 原作者库：https://github.com/zmqcherish
@@ -17,7 +17,7 @@
 ***********************************/	  
 
 
-const version = 'V2.0.16';
+const version = 'V2.0.17';
 
 /*主要的选项配置*/
 const mainConfig = {
@@ -110,7 +110,6 @@ const otherUrls = {
 	'wbapplua/wbpullad.lua': 'removeLuaScreenAds',   			//Lua开屏广告
 	'interface/sdk/sdkad.php': 'removePhpScreenAds',  			//Php开屏广告
 	'ct=feed&a=trends': 'removeTopics',                         /* 国际版屏蔽探索页面下的一些Topic */
-	'search_topic'    : 'modifiedSearchTopic',                  /*国际版SearchTopic*/
 	'user_center'     : 'modifiedUserCenter'                    /*国际版用户中心*/
 }
 
@@ -132,11 +131,6 @@ function getModifyMethod(url) {
 		}
 	}
 	return null;
-}
-/*国际版SearchTopic*/
-function modifiedSearchTopic(data) {
-	data = {"data":[],"info":"","retcode":0,"ext":{"search_hot_simple":{"title":"\u70ed\u95e8\u641c\u7d22","desc":"","more":"\u66f4\u591a\u70ed\u641c"},"search_hot":{"title":"\u5fae\u535a\u70ed\u641c\u699c","desc":"\u5b9e\u65f6\u70ed\u70b9\uff0c\u6bcf\u5206\u949f\u66f4\u65b0\u4e00\u6b21","more":""},"search_city":{"title":"\u540c\u57ce\u70ed\u70b9","desc":"","more":""}}};
-	return data;
 }
 /*国际版用户中心*/
 function modifiedUserCenter(data) {
