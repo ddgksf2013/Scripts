6dcf44d9f4725cbcb3c374972edea18e819cb532
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2022-12-17 16:42:14 +0800

    Create clear_qxdns_cache.js

---
+++ clear_qxdns_cache.js
@@ -0,0 +1,30 @@
+/*
+
+针对QuantumultX下载AppleStore应用速度过慢的几种解决措施：
+
+1、为apple添加代理分流
+
+2、禁用配置文件【DNS】下的no-system或将【DNS】下内容全部删除
+
+3、清除QX的DNS缓存，添加定时脚本from@unknowntokyo(每隔12清理一次）
+
+0 */12 * * * https://github.com/ddgksf2013/Scripts/raw/master/clear_qxdns_cache.js
+
+以上三种方式，可自行尝试，选择适合自己的使用
+
+*/
+
+const message = {
+    action: "dns_clear_cache"
+};
+
+$configuration.sendMessage(message).then(resolve => {
+    if (resolve.ret) {
+        console.log("dnsCache Cleared!");
+    } else {
+        console.log(resolve.error);
+    }
+    $done();
+}, reject => {
+    $done();
+});
