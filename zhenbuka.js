8c3b633399ca7a2e07bd171590fc1fe516ba05cd
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2022-12-25 22:07:11 +0800

    Update zhenbuka.js

--- zhenbuka.js
+++ zhenbuka.js
@@ -1,3 +1,3 @@
 /* 公众号墨鱼手记 crated by ddgksf2013 on 2022-05-28 */
-var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/zhenbuka.css" type="text/css"><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/zhenbuka.js"></script>').replace(/'6572'/g, "'6578'");
+var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/zhenbuka.css" type="text/css"><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/ddgksf2013/Html@main/zhenbuka.js"></script>').replace(/'6572'/g, "'6578'");
 $done({ body });
