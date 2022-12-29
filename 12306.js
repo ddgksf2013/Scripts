09234291ad8c40a4ffb13212b9b556ea4857d704
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2022-12-29 11:52:33 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -4,9 +4,7 @@ https://t.me/ddgksf2013
 
 12306 去除倒计时 made by ddgksf2013 on 2022-12-29
 
-请检查你的分流是否有ad.12306.cn，如果有，需要去除（可使用解析器排出#out=12306），不然重写是无法生效的，同时检查是否有与其冲突的重写
-
-或者在本地添加分流 host, ad.12306.cn, direct
+使用本重写，请务必在本地添加分流 host, ad.12306.cn, direct
 
 [rewrite_local]
 
