488cf4b0594b44eae4cf1ec2384037abf3e6cd72
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-01-05 14:05:33 +0800

    Update startup.js

--- startup.js
+++ startup.js
@@ -1,8 +1,9 @@
 
 
 
-/*京喜APP*/
+
 let obj=JSON.parse($response.body);
+/*京喜APP*/
 if (/^https?:\/\/api\.m\.jd\.com\/api\?functionId=delivery_show/.test($request.url)) { 
     obj.data.materialList.startTime=3667476800000;
     obj.data.materialList.endTime  =3667908800000;
