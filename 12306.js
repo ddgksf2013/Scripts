83b0ddc623529468617023736c769e912adbc9d0
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-01-17 13:25:30 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -1,5 +1,5 @@
 /***********************************************
-> 应用名称：墨鱼自用B站去广告脚本
+> 应用名称：墨鱼自用12306去广告脚本
 > 脚本作者：@ddgksf2013
 > 微信账号：墨鱼手记
 > 更新时间：2023-01-17
@@ -31,6 +31,6 @@ hostname = ad.12306.cn
 
 
 
-const version = 'V1.0.17';
+const version = 'V1.0.18';
 
 var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList&&(1==ddgksf2013.materialsList.length?(ddgksf2013.materialsList[0].filePath="https://api.dujin.org/bing/m.php",ddgksf2013.advertParam.skipTime=1,delete ddgksf2013.materialsList[0].billId,ddgksf2013.materialsList[0].billMaterialsId="6491"):ddgksf2013.materialsList.length>1&&(ddgksf2013.materialsList=[{}])),$done({body:JSON.stringify(ddgksf2013)});
