63d38981638af39ddde549f03c588ae88c4d76d2
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-02-04 19:09:20 +0800

    Create zhihu_openads.js

---
+++ zhihu_openads.js
@@ -0,0 +1,50 @@
+/***********************************
+
+> 应用名称：知乎开屏
+> 脚本作者：ddgksf2013
+> 微信账号：墨鱼手记
+> 更新时间：2023-02-04
+> 通知频道：https://t.me/ddgksf2021
+> 投稿助手：https://t.me/ddgksf2013_bot
+> 脚本功能：去开屏广告
+> 问题反馈：📮 ddgksf2013@163.com 📮
+> 特别说明：⛔⛔⛔
+           本脚本仅供学习交流使用，禁止转载售卖
+           ⛔⛔⛔
+		   
+		   
+请搭配@blackmatrix7的知乎净化助手使用
+https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/zheye/zheye.snippet
+
+
+[rewrite_local]
+
+# ～ 知乎开屏（2023-02-04）@ddgksf2013
+^https?:\/\/api\.zhihu\.com\/commercial_api.*launch_v2 url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/zhihu_openads.js
+
+[mitm]
+hostname=gg.caixin.com
+
+***********************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var body=$response.body.replace(/img_play_duration\\":\d+/g,'img_play_duration":0').replace(/launch_timeout\\":\d+/g,'launch_timeout":0');$done({body});
