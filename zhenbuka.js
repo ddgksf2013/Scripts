f91b86fe8a3bd655fa2c66383b56d1e71d7f638f
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-02-12 21:25:51 +0800

    Update zhenbuka.js

--- zhenbuka.js
+++ zhenbuka.js
@@ -1,3 +1,3 @@
-/* 公众号墨鱼手记 crated by ddgksf2013 on 2022-02-10 */
-var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/zhenbuka.css" type="text/css">').replace(/'6572'/g, "'6578'").replace(/\<div  class=\"item stui-banner__item[\s\S]*html[\s\S]*?\<\/div\>/g, "").replace(/img id="ik\d+"/g, 'img id="ddgksf2013"');
+/* 公众号墨鱼手记 crated by ddgksf2013 on 2022-02-12 */
+var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/zhenbuka.css" type="text/css">').replace(/'159140'/g, "'259140'").replace(/\<div  class=\"item stui-banner__item[\s\S]*html[\s\S]*?\<\/div\>/g, "").replace(/img id="ik\d+"/g, 'img id="ddgksf2013"');
 $done({ body });
