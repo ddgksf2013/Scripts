1c8432b611b7d4c99086443745ca61997fbcd6fb
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-02-21 22:08:05 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -1,8 +1,9 @@
 /***********************************************
+
 > 应用名称：墨鱼自用12306去广告脚本
 > 脚本作者：@ddgksf2013
 > 微信账号：墨鱼手记
-> 更新时间：2023-01-17
+> 更新时间：2023-02-21
 > 通知频道：https://t.me/ddgksf2021
 > 贡献投稿：https://t.me/ddgksf2013_bot
 > 问题反馈：ddgksf2013@163.com
@@ -31,6 +32,6 @@ hostname = ad.12306.cn
 
 
 
-const version = 'V1.0.18';
+const version = 'V1.0.19';
 
-var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList&&(1==ddgksf2013.materialsList.length?(ddgksf2013.materialsList[0].filePath="https://api.dujin.org/bing/m.php",ddgksf2013.advertParam.skipTime=1,delete ddgksf2013.materialsList[0].billId,ddgksf2013.materialsList[0].billMaterialsId="6491"):ddgksf2013.materialsList.length>1&&(ddgksf2013.materialsList=[{}])),$done({body:JSON.stringify(ddgksf2013)});
+var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},delete ddgksf2013.message,$done({body:JSON.stringify(ddgksf2013)});
