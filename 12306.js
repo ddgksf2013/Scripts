baa91675431ec30f3f8602e15bd255920976d8f8
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-02-22 17:52:26 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -33,6 +33,6 @@ hostname = ad.12306.cn
 
 
 
-const version = 'V1.0.21';
+const version = 'V1.0.22';
 
-var obj=JSON.parse($request.body),ddgksf2013={};console.log($request.body),"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
