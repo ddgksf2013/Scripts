875e8f0f5c36482d89a422e1ba3f64f71e239c55
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-02-22 09:05:40 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -3,7 +3,7 @@
 > 应用名称：墨鱼自用12306去广告脚本
 > 脚本作者：@ddgksf2013
 > 微信账号：墨鱼手记
-> 更新时间：2023-02-21
+> 更新时间：2023-02-22
 > 通知频道：https://t.me/ddgksf2021
 > 贡献投稿：https://t.me/ddgksf2013_bot
 > 问题反馈：ddgksf2013@163.com
@@ -12,7 +12,7 @@
 
 [rewrite_local]
 
-^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/12306.js
+^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-analyze-echo-response https://github.com/ddgksf2013/Scripts/raw/master/12306.js
 
 [mitm]
 
@@ -32,6 +32,6 @@ hostname = ad.12306.cn
 
 
 
-const version = 'V1.0.19';
+const version = 'V1.0.20';
 
-var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},delete ddgksf2013.message,$done({body:JSON.stringify(ddgksf2013)});
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013={code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
