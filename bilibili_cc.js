432af874df76d7354e35abf2a70a91538cf9bee9
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-04-11 17:49:24 +0800

    Update bilibili_cc.js

--- bilibili_cc.js
+++ bilibili_cc.js
@@ -5,7 +5,7 @@
 > 下载地址：https://apps.apple.com/cn/app/id736536022
 > 脚本作者：Cuttlefish
 > 微信账号：墨鱼手记
-> 更新时间：2022-11-22
+> 更新时间：2023-04-11
 > 通知频道：https://t.me/ddgksf2021
 > 投稿助手：https://t.me/ddgksf2013_bot
 > 问题反馈：📮 ddgksf2013@163.com 📮
@@ -13,12 +13,12 @@
  
 [rewrite_local]
 
-# ～ 嗶哩嗶哩繁体CC字幕转中文简体（2022-10-12）@ddgksf2013
-^https?:\/\/i.\.hdslb\.com\/bfs\/subtitle\/.+\.json$ url script-response-body https://github.com/ddgksf2013/Scripts/raw/main/bilibili_cc.js
+# ～ 嗶哩嗶哩繁体CC字幕转中文简体（2023-04-11）@ddgksf2013
+^https?:\/\/aisubtitle.hdslb\.com\/bfs\/subtitle\/.+\.json$ url script-response-body https://github.com/ddgksf2013/Scripts/raw/main/bilibili_cc.js
 
 [mitm]
 
-hostname=i0.hdslb.com
+hostname=aisubtitle.hdslb.com
 
 ***********************************/
 
