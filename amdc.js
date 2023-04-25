2319158bd7b0f04c6f26e68eb16a8941cee48e35
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-04-25 22:37:13 +0800

    Update amdc.js

--- amdc.js
+++ amdc.js
@@ -10,4 +10,7 @@
 ***********************************************/
 
 
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];-1!=ua.indexOf("AMap")||-1!=ua.indexOf("Cainiao")?$done({body:"ddgksf2013"}):$done({});
+
+const version = 'V1.0.3';
+
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];-1!=ua.indexOf("AMap")||-1!=ua.indexOf("Cainiao")||-1!=ua.indexOf("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C")?$done({body:"ddgksf2013"}):$done({});
