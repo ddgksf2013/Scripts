dc11d01a437e891936b961cd5fc323dc85268ccc
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-06-03 09:48:26 +0800

    Update cupbox.js

--- cupbox.js
+++ cupbox.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html@main/cupbox.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/cupbox.css" type="text/css">');
 $done({ body });
