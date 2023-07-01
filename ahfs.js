9b288826513a15e40b1622e496db7aad582fd61d
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-07-01 22:04:51 +0800

    Update ahfs.js

--- ahfs.js
+++ ahfs.js
@@ -26,5 +26,28 @@ hostname = *.ahhhhfs.com
 
 
 
-var body = $response.body.replace(/<head>/, '<head><style>.top, .bottum, .sidebar-column, .m-menubar, .google-auto-placed, #ad_position_box{display:none!important} </style>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var body = $response.body.replace(/<head>/, '<head><style>.top, .bottum, .sidebar-column, .m-menubar, .google-auto-placed, #rizhuti_v2_module_division-2, #related_posts, .post-note, .entry-share, .related-posts, #comments, #ad_position_box{display:none!important} </style>');
 $done({ body });
