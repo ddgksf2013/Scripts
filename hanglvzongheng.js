f0a6291c69801ee942000b98a785f6ef4cec5950
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2023-10-27 12:15:21 +0800

    Update hanglvzongheng.js

--- hanglvzongheng.js
+++ hanglvzongheng.js
@@ -12,4 +12,4 @@
 
 const version = 'V1.0.1';
 
-var ua=$request.headers.rpid||$request.headers.Rpid;ua.includes("1000002")||ua.includes("1000019")?$done({status:"HTTP/1.1 404 Not Found"}):$done({});
+var ua=$request.headers.rpid||$request.headers.Rpid;ua.includes("10000012")||ua.includes("1000019")?$done({status:"HTTP/1.1 404 Not Found"}):$done({});
