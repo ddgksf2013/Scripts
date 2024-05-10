4d6dca899737309d803b8dab7b5177543cb5961a
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2024-05-10 20:56:57 +0800

    Update baishitv.js

--- baishitv.js
+++ baishitv.js
@@ -1,1 +1,1 @@
-var obj=JSON.parse($response.body);obj.dt.data=Object.values(obj.dt.data).filter(a=>-1!==a.jumpTypeString.indexOf("广告")),$done({body:JSON.stringify(obj)});
+var obj=JSON.parse($response.body);obj.dt.data=Object.values(obj.dt.data).filter(a=>-1===a.jumpTypeString.indexOf("广告")),$done({body:JSON.stringify(obj)});
