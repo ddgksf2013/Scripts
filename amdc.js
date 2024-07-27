05104ff9c3142f234eb705a147a91d1ddd327746
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2024-07-27 13:00:48 +0800

    Update amdc.js

--- amdc.js
+++ amdc.js
@@ -2,7 +2,7 @@
 > 应用名称：墨鱼自用amdc脚本
 > 脚本作者：@ddgksf2013
 > 微信账号：墨鱼手记
-> 更新时间：2024-01-21
+> 更新时间：2024-07-27
 > 通知频道：https://t.me/ddgksf2021
 > 贡献投稿：https://t.me/ddgksf2013_bot
 > 问题反馈：ddgksf2013@163.com
@@ -11,8 +11,8 @@
 
 //使用AMDC的APP有：咸鱼、淘宝、盒马、高德、菜鸟、飞猪
 
-const version = 'V1.0.8';
+const version = 'V1.0.9';
 
 
 
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];/(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|Hema4iPhone|Moon|DMPortal)/.test(ua)?$done({body:"ddgksf2013"}):$done({});
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];/(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|Hema4iPhone|Moon|DMPortal)/.test(ua)?$done({body:"ddgksf2013"}):$done({});
