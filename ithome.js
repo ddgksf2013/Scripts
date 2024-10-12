74d9962cd09a9598aa93ff945fa83e55f07046bc
Cuttlefish <116851128+ddgksf2013@users.noreply.github.com>
2024-10-12 18:33:24 +0800

    Create ithome.js

---
+++ ithome.js
@@ -0,0 +1,14 @@
+/***********************************************
+> 应用名称：ithome去除信息流广告
+> 脚本作者：@ddgksf2013
+> 微信账号：墨鱼手记
+> 更新时间：2024-10-12
+> 通知频道：https://t.me/ddgksf2021
+> 贡献投稿：https://t.me/ddgksf2013_bot
+> 问题反馈：ddgksf2013@163.com
+> 特别提醒：如需转载请注明出处，谢谢合作！
+***********************************************/
+
+const version = 'V1.0.1';
+
+var obj=JSON.parse($response.body);obj.data.list=Object.values(obj.data.list).filter(a=>2!=a.feedContent.flag),$done({body:JSON.stringify(obj)});
